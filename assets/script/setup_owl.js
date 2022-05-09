f2c0cd37a0ed60b16922bd931ad767125f4534fb
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-09 20:26:55 -0300

    Cards e owl carousel

---
+++ assets/script/setup_owl.js
@@ -0,0 +1,16 @@
+$('#slider').owlCarousel({
+  center: true,
+  loop: true,
+  margin: 15,
+  nav: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 2
+    },
+
+    800: {
+      items: 2.5
+    }
+  }
+})
